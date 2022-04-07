ff4971b5ec0c0937f010410c796f90a8d97dcdb6
maryam-tanveer <maryamtanveer570@gmail.com>
2022-04-07 11:05:24 -0700

    initial commit

---
+++ src/test/java/com/multilevel/inheritance/InheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multilevel.inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
